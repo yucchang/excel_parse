fd33a2e4af1a3034771ff91dcdb9856844f00188
Justin Chang <justinyu711@gmail.com>
2020-04-20 00:02:33 +0800

    Invoice delete function

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,8 +1,15 @@
 class InvoicesController < ApplicationController
+
   def index
     @invoices = Invoice.all
     respond_to do |format|
       format.html
     end
+  end
+
+  def destroy
+    invoice = Invoice.find(params[:id])
+    invoice.delete if invoice
+    redirect_to invoices_path, notice: 'Invoice removed.'
   end
 end
